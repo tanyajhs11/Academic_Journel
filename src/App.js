06e6aa6e173fa88360ed9b46e72bb4b94bec849f
Ashutosh Sharma <iamashu2002@gmail.com>
2023-03-24 13:56:35 +0530

    Implemented Components Sync

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import './App.css';
-import Sidebar from './Components/Sidebar';
-import { BrowserRouter, Link, Route ,Routes} from 'react-router-dom'
-import Bookchapter from './Components/Bookchapter';
-import Journal from './Components/Journal';
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
+import React, { useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Bookchapter from "./Components/Bookchapter";
+import Journal from "./Components/Journal";
+import Conference from "./Components/Conference";
+import AdministrationDevProg from "./Components/AministrationDevProg";
+import FacultyDevProg from "./Components/FacultyDevProg";
+import InvitedTalks from "./Components/InvitedTalks";
+import ExpertTalks from "./Components/ExpertTalks";
+import PaperReviews from "./Components/PaperReviews";
+import WorkshopSeminar from "./Components/WorkshopSeminar";
 
 const App = () => {
-  
-
+  const [toRender, setToRender] = useState("Journal");
+  const myMap = {
+    Journal: <Journal />,
+    Bookchapter: <Bookchapter />,
+    Conference: <Conference />,
+    ExpertTalks: <ExpertTalks />,
+    InvitedTalks: <InvitedTalks />,
+    WorkshopSeminar: <WorkshopSeminar />,
+    FacultyDevProg: <FacultyDevProg />,
+    AdministrationDevProg: <AdministrationDevProg />,
+    PaperReviews: <PaperReviews />,
+  };
   return (
-  <div className="App">
-    <Sidebar/>
-  </div>
-  )
-
-
-  
-}
+    <div className="App">
+      <div className="container">
+        <Sidebar toRender={toRender} setToRender={setToRender} />
+        {myMap[toRender]}
+      </div>
+    </div>
+  );
+};
 
 export default App;
-
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,35 +1,29 @@
-import React from 'react'
+import React from "react";
 import "../App.css";
-import { SidebarData} from './SidebarData';
+import { SidebarData } from "./SidebarData";
 
+function Sidebar({ toRender, setToRender }) {
+  return (
+    <div className="Sidebar">
+      <ul className="SidebarList">
+        {SidebarData.map((val, key) => {
+          return (
+            <li
+              key={key}
+              className="row"
+              //id={window.location.pathname=val.link ? "active" : ""}
+              onClick={() => {
+                setToRender(val.title);
+              }}
+            >
+              {" "}
+              <div id="icon">{val.icon}</div> <div id="title">{val.title}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
 
-function Sidebar() {
-    return (
-      <div className="Sidebar">
-          <ul className="SidebarList">
-              {SidebarData.map((val,key)=>{
-                  return (
-                  <li 
-                  key={key}
-                  className="row"
-                  //id={window.location.pathname=val.link ? "active" : ""}
-                   onClick={()=>{
-                    window.location.pathname=val.link;
-                }}
-                >
-                    
-                      {" "}
-                      <div id="icon">{val.icon}</div>{" "}
-                          <div id="title">
-                              {val.title}
-                          </div>
-                  </li>
-                  );
-              })}
-          </ul>
-      </div>
-    );
-  }
-      
-  
-  export default Sidebar
+export default Sidebar;
